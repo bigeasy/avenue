4a82fe1376559f4f9428aa1e1d71ecf3394a93c6
Alan Gutierrez <alan@prettyrobots.com>
2013-07-25 00:04:10 -0400

    Initial commit.
    Closes #2.

---
+++ index.js
@@ -0,0 +1,64 @@
+var fs = require('fs')
+var path = require('path')
+
+function routes (base, suffix) {
+    var routes = []
+    var dotted = '.' + suffix
+
+    function children (base, parts) {
+        var dir = path.join.apply(path, [ base ].concat(parts))
+        var files = []
+
+        fs.readdirSync(dir).forEach(function (entry) {
+            var file = path.join(dir, entry), stat = fs.statSync(file)
+            if (stat.isDirectory()) {
+                children(base, parts.concat(entry))
+            } else {
+                files.push(entry)
+            }
+        })
+        files.forEach(function (entry) {
+            var file = path.join(dir, entry)
+            var suffixed
+            if (entry.lastIndexOf(dotted) == entry.length - dotted.length && entry.lastIndexOf(dotted) > 0) {
+                if (entry[0] != '_') {
+                    var $ = /^(.*?)(_?)\.(.*)$/.exec(entry)
+                    var name = $[1]
+                    var pathInfo = !! $[2]
+                    var extension = $[3]
+                    var route = parts.slice(0)
+
+                    if (name != 'index') route.push(name)
+
+                    // Note that we do not use the file system's path separator when
+                    // resolving stencils.
+                    routes.push({
+                        route: '/' + route.join('/'),
+                        script: parts.concat(entry).join('/'),
+                        path: route.slice(),
+                        file: entry,
+                        name: name,
+                        extension: extension.slice(0, - dotted.length)
+                    })
+
+                    if (pathInfo) {
+                        routes.push({
+                            route: '/' + route.join('/') + '/**:pathInfo',
+                            script: parts.concat(entry).join('/'),
+                            path: route.slice(),
+                            file: entry,
+                            name: name,
+                            extension: extension.slice(0, - dotted.length)
+                        })
+                    }
+                }
+            }
+        })
+    }
+
+    children(base, [])
+
+    return routes
+}
+
+module.exports = routes
---
+++ t/avenue/find.t.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+require('proof')(1, function (ok, deepEqual) {
+    var find = require('../..'), path = require('path')
+    var found = find(path.join(__dirname, 'fixtures'), 'js')
+    found.sort(function (a, b) { return a.path < b.path ? -1 : a.path > b.path ? 1 : 0 })
+    deepEqual(found, [ { route: '/directory',
+                         script: 'directory/index.get.js',
+                         path: [ 'directory' ],
+                         file: 'index.get.js',
+                         name: 'index',
+                         extension: 'get' },
+                       { route: '/exact',
+                         script: 'exact.js',
+                         path: [ 'exact' ],
+                         file: 'exact.js',
+                         name: 'exact',
+                         extension: '' },
+                       { route: '/pathed',
+                         script: 'pathed_.get.js',
+                         path: [ 'pathed' ],
+                         file: 'pathed_.get.js',
+                         name: 'pathed',
+                         extension: 'get' },
+                       { route: '/pathed/**:pathInfo',
+                         script: 'pathed_.get.js',
+                         path: [ 'pathed' ],
+                         file: 'pathed_.get.js',
+                         name: 'pathed',
+                         extension: 'get' } ], 'found')
+})
